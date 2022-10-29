ab83a7ed5071dbabf84492678114847d97a4e1a2
Juan Sebastian Martinez Soto <sebasmarti1997@gmail.com>
2022-10-29 11:52:25 -0500

    [ADD] Agregando primera sección de busqueda de usuarios con listados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-prueba-dvp';
+
+  users: any[] = [];
+  search_string = "" ;
+
+  constructor(
+  private http: HttpClient
+  ){}
+
+  ngOnInit(){
+
+  }
+
+  searchUserByName(){
+  	 this.http.get('https://api.github.com/search/users?q='+this.search_string).subscribe( (data: any) => {
+  		if(!data.errors){
+  			this.users=data.items.slice(0,10);
+  		}
+  	});
+  }
 }
